60009034a39bc0a395a265568765990fd891d4b1
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-04 23:37:30 +0200

    enhance the style

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
-        {children}
-        <Footer/>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
